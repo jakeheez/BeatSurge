a2e65fbefb4508d8c16bf5d2d02b4d9970355092
JHeaser <jakeheez@gmail.com>
2017-12-11 00:02:48 -0600

    2017/12/11 - Replace top nav bar with custom bar, start to title-section, update css primary / secondary color input from js

--- Scripts/beatPage.js
+++ Scripts/beatPage.js
@@ -10,14 +10,19 @@ this.updateViewOnLoad = function (modelJson) {
 this.updateViewInfo = function () {
 	document.body.style.backgroundColor = model.Producer.PrimaryColor;
 
-	var beatButtons = document.getElementsByClassName("beat-button");
+	var beatButtons = document.getElementsByClassName("primary-colored");
 	for (var i = 0; i < beatButtons.length; i++) {
 		beatButtons[i].style.backgroundColor = model.Producer.PrimaryColor;
 	}
 
-	var beatContainers = document.getElementsByClassName("beat-container");
+	var beatContainers = document.getElementsByClassName("secondary-colored");
 	for (var j = 0; j < beatContainers.length; j++) {
 		beatContainers[j].style.backgroundColor = model.Producer.SecondaryColor;
+	}
+
+	var textColorItems = document.getElementsByClassName("text-colored");
+	for (var k = 0; k < textColorItems.length; k++) {
+		textColorItems[k].style.color = model.Producer.TextColor;
 	}
 };
 
